0aee9316830fe9ded8b55277454102e75d84b989
kawaiierror <sunnata30@gmail.com>
2026-02-03 13:58:04 +0700

    Added new computed proprety, practice 7 done

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: "Vue Mastery",
         description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        selectedVariant: 0,
         altText: "Носочки",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -14,14 +14,16 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
-
         ],
+
         cart: 0,
 
     },
@@ -34,8 +36,23 @@ let app = new Vue({
                 this.cart -= 1
             }
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        Sale() {
+            if (this.onSale === true) return this.brand + " product" + " is on Sale!";
         },
     },
 })
